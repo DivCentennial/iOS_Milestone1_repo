44a6c5ad1c84e7dd425369b63de903d1593b3b67
kashishksy <kashishksy2112@gmail.com>
2024-10-31 20:56:04 -0400

    pushing changes for screen 1

---
+++ MAPD714-002_TeamProject_Team10/ContentView.swift
@@ -0,0 +1,50 @@
+//
+//  ContentView.swift
+//  MAPD714-002_TeamProject_Team10
+//
+//  Created by Kashish Yadav on 2024-10-31.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    var body: some View {
+        VStack(spacing: 20) {
+            
+            // Logo
+            Image("cenphone_logo") // Replace with your actual image name
+                .resizable()
+                .frame(width: 400, height: 360)
+            
+            // Slogan
+//            Text("Cenphone - The Future of Mobile")
+//                .font(.title)
+//                .multilineTextAlignment(.center)
+            
+            // Order Now Button
+            Button(action: {
+                // Action to navigate to the next screen
+            }) {
+                Text("Order Now")
+                    .font(.headline)
+                    .frame(width: 200, height: 50)
+                    .background(Color.white)
+                    .foregroundColor(Color(UIColor(red: 191/255, green: 56/255, blue: 125/255, alpha: 1.0))) // Sets text color to #BF387D
+                    .cornerRadius(8)
+            }
+        }
+        .padding(.top, 34)
+               .frame(maxWidth: .infinity, maxHeight: .infinity) // Makes the VStack take the full screen
+               .background(Color(UIColor(red: 191/255, green: 56/255, blue: 125/255, alpha: 1.0))) // Sets background to #BF387D
+               .ignoresSafeArea() // Extends background to cover entire screen
+
+    }
+}
+
+
+
+#Preview {
+    ContentView()
+}
+
---
+++ MAPD714-002_TeamProject_Team10/MAPD714_002_TeamProject_Team10App.swift
@@ -0,0 +1,17 @@
+//
+//  MAPD714_002_TeamProject_Team10App.swift
+//  MAPD714-002_TeamProject_Team10
+//
+//  Created by Kashish Yadav on 2024-10-31.
+//
+
+import SwiftUI
+
+@main
+struct MAPD714_002_TeamProject_Team10App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
